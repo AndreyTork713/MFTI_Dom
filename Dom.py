228ad7cb16da252e51a0a9f6d155297668b62cd5
Andrey Kropotin <tork713@gmail.com>
2020-05-01 08:16:55 +0700

    Скилет программы с заглушками в функциях и появилось окно для рисования

--- Dom.py
+++ Dom.py
@@ -1,12 +1,34 @@
+from graph import *
+
+
 def main():
+    windowSize(600, 500)
     paint_house(100, 100, 50, 50)
+    run()
 
 
-def paint_house(x, y, wigth, heigth):
+def paint_walls(x, y, width, param):
+    pass
+
+
+def paint_roof(x, y, width, param):
+    pass
+
+
+def paint_window(param, param1, w_width, w_height):
+    pass
+
+
+def paint_house(x, y, width, heigth):
     """Рисует домик
     (x, y) - левая точка крыши
     """
-    pass
+    paint_walls(x, y, width, heigth // 2)
+    paint_roof(x, y, width, heigth // 2)
+
+    w_height = heigth // 6
+    w_width = width // 3
+    paint_window(x + w_width, y + w_height, w_width, w_height)
 
 
 main()
